18902bf1901ce7770d8e58bae2359d6bdb16c690
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-07 18:16:06 -0300

    update

--- mundo2/condicao-aninhada.py
+++ mundo2/condicao-aninhada.py
@@ -3,6 +3,8 @@ if nome == 'Nathan':
     print('Que nome bonito!')
 elif nome == 'José' or nome =='Maria' or nome == 'João':
     print('Seu nome é bem popular no Brasil!')
+elif nome in 'Ana Cláudia Jéssica Juliana':
+    print('Belo nome feminino!')
 else:
     print('Seu nome é bem normal!')
 
