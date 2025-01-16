25969e3fcd1298e0c74f5482064d8a6e23dedb92
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-16 13:17:04 -0300

    Create tuplas.py

---
+++ mundo3/tuplas.py
@@ -0,0 +1,19 @@
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c)
+print(c.index(8))
+
+pessoa = ('nathan', 25, 'M', 100)
+d = c + pessoa
+print(d)
+'''lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(sorted(lanche))'''
+
+'''for pos, comida in enumerate(lanche):
+    print(f'vou comer {comida} na posição {pos} ')
+print('explodi!')'''
+
+#for cont in range(0, len(lanche)):
+ #   print(f'vou comer {lanche[cont]}')
+#print('to cheio')
