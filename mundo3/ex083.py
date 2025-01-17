0922a08bf62bcfd875c4e992ddd56caae3eccd4b
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-17 17:16:40 -0300

    Update ex083.py

--- mundo3/ex083.py
+++ mundo3/ex083.py
@@ -1,3 +1,18 @@
 '''
 Ler expressão com parentes e reconhecer se está com parentes na ordem certa
 '''
+expr = str(input('Digite uma expressão: '))
+pilha = list()
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está inválida!')
