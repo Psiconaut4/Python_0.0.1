bb39d0823d5787a867484b7a97fc0244618a9544
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-17 16:45:24 -0300

    Update ex080.py

--- mundo3/ex080.py
+++ mundo3/ex080.py
@@ -1,6 +1,15 @@
 '''Inserir 5 números em ordem crescente sem uso do .sort()'''
 
 valores = []
-for n in range(0,5):
-    num = input('digite um valor: ')
-    valores.append(num)
+for c in range(0,5):
+    n = int(input('digite um valor: '))
+    if c == 0 or n > valores[-1]:
+        valores.append(n)
+    else:
+        pos = 0
+        while pos < len(valores):
+            if n <= valores[pos]:
+                valores.insert(pos, n)
+                break
+            pos += 1
+print(valores)
