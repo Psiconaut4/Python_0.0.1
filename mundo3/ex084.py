5300a6d078076323b166d2939aa522d060b5bcc6
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-18 18:06:42 -0300

    Update ex084.py

--- mundo3/ex084.py
+++ mundo3/ex084.py
@@ -8,13 +8,11 @@ dado = []
 pessoas = []
 pesado = []
 leve = []
-total = 0
 while True:
     dado.append(input('Nome: '))
     dado.append(float(input('Peso: ')))
     pessoas.append(dado[:])
     dado.clear()
-    total += 1
     resp = str(input('Quer continuar? [S/n]: '))
     if resp in 'Nn':
         break
@@ -23,6 +21,6 @@ for p in pessoas:
         pesado.append(p)
     elif p[1] <= 50:
         leve.append(p)
-print(f'Ao todo foram cadastradas {total} pessoas.')
+print(f'Ao todo foram cadastradas {len(pessoas)} pessoas.')
 print(f'as pessoas mais pesadas foram {pesado}.')
 print(f'E as mais leves foram {leve}.')
