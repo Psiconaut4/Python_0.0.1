45ca9a539b3cba41f716af9ac00abfffd932db4a
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-18 18:33:45 -0300

    Update ex086-87.py

--- mundo3/ex086-87.py
+++ mundo3/ex086-87.py
@@ -4,27 +4,19 @@ mostrar matriz na tela com formatação correta
 mostrar a soma dos valores pares
 a soma dos valores da terceira coluna
 o maior valor da segunda linha'''
-linha1 = []
-linha2 = []
-linha3 = []
-matrix = []
-somapar = 0
-somacoluna = 0
-for c in range(0, 3):
-    linha1.append(int(input(f'Digite um valor para [0, {c}]: ')))
-matrix.append(linha1)
-for c in range(0, 3):
-    linha2.append(int(input(f'Digite um valor para [1, {c}]: ')))
-matrix.append(linha2)
-for c in range(0, 3):
-    linha3.append(int(input(f'Digite um valor para [2, {c}]: ')))
-matrix.append(linha3)
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
 print('-=-'*20)
-for i, l in enumerate(matrix):
-    for p, v in enumerate(l):
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
+
+    '''for p, v in enumerate(l):
         if v % 2 == 0:
             somapar += v
     if p == 2:
         somacoluna += v
-print(somapar)
-print(somacoluna)
+'''
