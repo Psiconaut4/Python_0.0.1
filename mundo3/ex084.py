9e0b7ec482afa310a308040165d04c852b9db1e4
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-18 13:04:58 -0300

    Update ex084.py

--- mundo3/ex084.py
+++ mundo3/ex084.py
@@ -4,3 +4,25 @@ ler nome e peso de varias pessoas
 quantas pessoas foram cadastradas
 uma listagem com as pessoas mais pesadas
 uma lista com as pessoas mais leves'''
+dado = []
+pessoas = []
+pesado = []
+leve = []
+total = 0
+while True:
+    dado.append(input('Nome: '))
+    dado.append(float(input('Peso: ')))
+    pessoas.append(dado[:])
+    dado.clear()
+    total += 1
+    resp = str(input('Quer continuar? [S/n]: '))
+    if resp in 'Nn':
+        break
+for p in pessoas:
+    if p[1] >= 100:
+        pesado.append(p)
+    elif p[1] <= 50:
+        leve.append(p)
+print(f'Ao todo foram cadastradas {total} pessoas.')
+print(f'as pessoas mais pesadas foram {pesado}.')
+print(f'E as mais leves foram {leve}.')
