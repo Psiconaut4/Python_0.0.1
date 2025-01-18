c4acde993d0439c01e489eaab05dad39c1cfa0cd
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-18 20:23:26 -0300

    Update ex089.py

--- mundo3/ex089.py
+++ mundo3/ex089.py
@@ -1,4 +1,4 @@
-cadastros = []
+'''cadastros = []
 perfil = []
 dado = []
 while True:
@@ -12,6 +12,30 @@ while True:
     resp = input('Quer continuar? [S/N]')
     if resp in 'Nn':
         break
-print('-=-'*60)
-print('No. NOME                MÉDIA')
+print('-=-'*20)
+print(f'{'No.':<4}{'NOME':<10}{'MÉDIA':>10}')
 print('--'*20)
+'''
+
+cadastros = list()
+while True:
+    nome = str(input('Nome: ').capitalize())
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    cadastros.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar? [S/n]: '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'{'No.':<4}{'NOME':<10}{'MÉDIA':>10}')
+print('-'*30)
+for i, a in enumerate(cadastros):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>9.1f}')
+while True:
+    print('-'*35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        break
+    if opc <= len(cadastros) - 1:
+        print(f'Notas de {cadastros[opc][0]} são {cadastros[opc][1]}.')
