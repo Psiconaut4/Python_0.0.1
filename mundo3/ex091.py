b789a6698daa686a99870e236ee505b6c525203e
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-01-21 13:59:45 -0300

    Update ex091.py

--- mundo3/ex091.py
+++ mundo3/ex091.py
@@ -1,24 +1,51 @@
-'''
-quatro jogadores jogam dado e tenham resultados aleatórios, guardar em um dicionário. no final coloque esse dicionário em ordem sabendo que o vencedor tirou o maior número no dado.
-'''
-
 from random import randint
 from time import sleep
+from operator import itemgetter
 
-pessoas = []
+jogo = {input('Nome: '): randint(1, 6),
+        input('Nome: '): randint(1, 6),
+        input('Nome: '): randint(1, 6),
+        input('Nome: '): randint(1, 6)}
+ranking = []
+print('__'*20)
+for k, v in jogo.items():
+    print(f'{f"{k} tirou {v} no dado.":^40}')
+    sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('--'*20)
+print(f'{"== RANKING DOS JOGADORES ==":^40}')
+print('__'*20)
+for i, v in enumerate(ranking):
+    print(f'{f"{i+1}º lugar: {v[0]} com {v[1]}.":^40}')
+    sleep(0.5)
+'''
 for c in range(0, 4):
     jogador = {'nome': '', 'jogada': ''}
     jogador['nome'] = str(input('Nome do Jogador: '))
-    a = randint(0, 6)
-    jogador['jogada'] = a
-    pessoas.append(jogador)
+    jogador['jogada'] = int(randint(1, 6))
+    pessoas = jogador
+ranking = {}
+ranking = sorted(pessoas.items(), key=itemgetter(3))
+for 'nome', 'jogada' in pessoas.items():
+    print(f'o {"nome"} tirou {"jogada"} no dado.')
+print(ranking)'''
 
-for c, v in enumerate(pessoas):
-    if c == 0:
-        win = pessoas[c]['nome']
-    if c <= len(pessoas) and pessoas[c]['jogada'] > pessoas[c-1]['jogada']:
-        win = pessoas[c]['nome']
-    sleep(1)
-    print(f'o dado para o jogador {pessoas[c]["nome"]} caiu em {pessoas[c]["jogada"]}')
+
+'''for c, v in enumerate(pessoas):
+if c == 0:
+    ranking['1lugar'] = pessoas[c]
+elif c <= len(pessoas) and pessoas[c]['jogada'] > pessoas[c-1]['jogada']:
+    ranking['1lugar'] = pessoas[c]
+elif pessoas[c]['jogada'] < ranking['1lugar']['jogada'] and pessoas[c]['jogada'] > pessoas[c-1]['jogada']:
+    ranking['2lugar'] = pessoas[c]
+elif pessoas[c]['jogada'] < pessoas[c-1]['jogada']:
+    ranking['3lugar'] = pessoas[c]
+else:
+    ranking['4lugar'] = pessoas[c]
+'''
+
+'''print(f'o dado para o jogador {pessoas[c]["nome"]} caiu em {pessoas[c]["jogada"]}')
 print('-=-'*20)
-print(f'o {win} venceu.')
+sleep(1)
+for c, v in enumerate(pessoas):
+    print(pessoas[c])'''
